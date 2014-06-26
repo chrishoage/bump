e035855776f2bd22493135ce711479fb92bff9d1
choage <iam@chrishoage.com>
2014-06-26 16:27:35 -0700

    setupConnection method on palyer

--- js/sprites/player.js
+++ js/sprites/player.js
@@ -4,10 +4,28 @@ var Player = (function () {
 		x = x || game.world.centerX;
 		y = y || game.world.centerY;
 	  Phaser.Sprite.call(this, game, x, y, asset, frame);
+
+	  this.playerReady = false;
 	}
 
 	Player.prototype = Object.create(Phaser.Sprite.prototype);
 	Player.prototype.constructor = Player;
+
+	Player.prototype.setupConnection = function (connection) {
+		var _this = this;
+		this.peerConn = connection;
+		this.peerConn.on('data', function (data) {
+			if (data.type === 'click') {
+				if (_this.playerReady) {
+					// trigger player power
+				} else {
+					_this.playerReady = true;
+				}
+			} else if (data.type === 'devicemotion') {
+				// device motion stuff here
+			}
+		});
+	});
 
 	return Player;
 
